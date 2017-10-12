9a4d94172ef5e566abdc50e33b3f4f31a8fcf385
Nick Strayer <nick@nicks-mbp.dhcp.mc.vanderbilt.edu>
2017-10-12 17:22:42 -0500

    sending to node

--- src/slid3r.js
+++ src/slid3r.js
@@ -1,3 +1,5 @@
+const d3 = require('d3');
+
 import {roundEndsStyle, trackStyle, trackInsetStyle, trackOverlayStyle, handleStyle} from './styles.js';
 
 // The function onDrag gets constantly fed slider position
@@ -14,98 +16,102 @@ function slid3r() {
       xPos = 0.5,
       yPos = 0.5,
       intClamp = true, // clamp handle to nearest int?
+      numTicks = 10,
       font = 'optima',
-      transitionLength = 200;
+      vertical = false,
+      transitionLength = 10;
  
-  
-  function drawSlider(selection) {
-    
+  function drawSlider(sel) {
+    // console.log('drawing slider')
+    if(vertical) console.log('rotating the slider');
+
+    // console.log(selection)
     const trans = d3.transition('sliderTrans').duration(transitionLength);
+  
+    const xScale = d3.scaleLinear()
+      .domain(sliderRange)
+      .range([0, sliderWidth])
+      .clamp(true);
+    
+    const getValue = (eventX) => xScale.invert(eventX);
+    
+    const slider = sel
+      .attr("class", "slider")
+      .attr("transform", `translate(${xPos}, ${yPos})`);
+    
+    if(vertical){
+      slider.attr("transform", `rotate(90)`);
+    }
+    
+    const track = slider.selectAppend("line.track")
+      .attr('x1',xScale.range()[0])
+      .attr('x2', xScale.range()[1]);
 
-    selection.each(function(d, i) {
-      const sel = d3.select(this);
-      
-      const xScale = d3.scaleLinear()
-        .domain(sliderRange)
-        .range([0, sliderWidth])
-        .clamp(true);
-      
-      const getValue = (eventX) => xScale.invert(eventX);
-      
-      const slider = sel
-        .attr("class", "slider")
-        .attr("transform", `translate( ${xPos}, ${yPos})`);
-      
-      const track = slider.append("line")
-          .attr("class", "track")
-          .attr('x1',xScale.range()[0])
-          .attr('x2', xScale.range()[1])
-         
-      const trackInset = track.select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-          .attr("class", "track-inset")
-      
-      
-      const trackOverlay = trackInset
-        .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-          .attr("class", "track-overlay")
+    const trackInset = slider.selectAppend('line.track-inset')
+      .attr('x1',xScale.range()[0])
+      .attr('x2', xScale.range()[1]);
 
-      trackOverlay
-          .call(
-            d3.drag()
-              .on("start.interrupt", function() { slider.interrupt(); })
-              .on("start drag", dragBehavior)
-              .on("end", finishBehavior)
-          )
-          
-      slider.insert("g", ".track-overlay")
-          .attr("class", "ticks")
-          .style('font', `10px ${font}`)
-          .attr("transform", "translate(0," + 18 + ")")
-        .selectAll("text")
-        .data(xScale.ticks(10))
-        .enter().append("text")
-          .attr("x", xScale)
-          .attr("text-anchor", "middle")
-          .text(d => d);
-      
-      const handle = slider.insert("circle", ".track-overlay")
-          .attr("class", "handle")
-          .attr("r", 9)
-          .attr("cx", xScale(startPos));
-          
+    const trackOverlay = slider.selectAppend('line.track-overlay')
+      .attr('x1',xScale.range()[0])
+      .attr('x2', xScale.range()[1]);
+    
+    trackOverlay
+        .call(
+          d3.drag()
+            .on("start.interrupt", function() { slider.interrupt(); })
+            .on("start drag", dragBehavior)
+            .on("end", finishBehavior)
+        )
+    
+    const tickFormat = xScale.tickFormat(5, intClamp ? ",.0f": 'f');
 
-      // write the label
-      slider.append('text')
-        .attr('y', -14)
-        .attr('font-family', font)
-        .text(label);
-          
-      // apply styles to everything.
-      roundEndsStyle(track);
-      trackStyle(track);
-      handleStyle(handle);
-      roundEndsStyle(trackOverlay);
-      trackOverlayStyle(trackOverlay);
-      roundEndsStyle(trackInset);
-      trackInsetStyle(trackInset);
-      
-      // setup callbacks
-      function dragBehavior(){
-        const scaledPos = getValue(d3.event.x);
-        // by inverting and reverting the position we assert bounds on the slider.
-        handle.attr("cx", xScale(scaledPos))
-        onDrag? onDrag(scaledPos): null;
-      }
-      
-      function finishBehavior(){
-        const dragPos = getValue(d3.event.x);
-        const finalPos = intClamp ? Math.round(dragPos): dragPos;
-        handle
-          .transition(trans)
-          .attr("cx", xScale(finalPos))
-        onDone(finalPos);
-      }
-    }); // end foreach loop
+    slider.selectAppend("g.ticks")
+        // .attr("class", "ticks")
+        .style('font', `10px ${font}`)
+        .attr("transform", "translate(0," + 18 + ")")
+      .selectAll("text")
+      .data(xScale.ticks(numTicks).map(tickFormat))
+      .enter().append("text.scaleTicks")
+        .attr("x", xScale)
+        .attr("text-anchor", "middle")
+        .text(d => d);
+    
+    const handle = slider.selectAppend("circle.handle")
+        .attr("class", "handle")
+        .attr("r", 9)
+        .attr("cx", xScale(startPos));         
+
+    // write the label
+    slider.selectAppend('text.label')
+      .attr('y', -14)
+      .attr('font-family', font)
+      .text(label);
+        
+    // apply styles to everything.
+    roundEndsStyle(track);
+    trackStyle(track);
+    handleStyle(handle);
+    roundEndsStyle(trackOverlay);
+    trackOverlayStyle(trackOverlay);
+    roundEndsStyle(trackInset);
+    trackInsetStyle(trackInset);
+    
+    // setup callbacks
+    function dragBehavior(){
+      const scaledPos = getValue(d3.event.x);
+      // by inverting and reverting the position we assert bounds on the slider.
+      handle.attr("cx", xScale(scaledPos))
+      onDrag? onDrag(scaledPos): null;
+    }
+    
+    function finishBehavior(){
+      const dragPos = getValue(d3.event.x);
+      const finalPos = intClamp ? Math.round(dragPos): dragPos;
+      handle
+        .transition(trans)
+        .attr("cx", xScale(finalPos))
+      onDone(finalPos);
+    }
   } // end drawSlider()
   
   // Getter and setters for changing settings.
@@ -155,6 +161,18 @@ function slid3r() {
   drawSlider.clamp = function(decision){
     if(!arguments.length) return intClamp;
     intClamp = decision;
+    return drawSlider;
+  }
+
+  drawSlider.vertical = function(decision){
+    if(!arguments.length) return vertical;
+    vertical = decision;
+    return drawSlider;
+  }
+
+  drawSlider.numTicks = function(num){
+    if(!arguments.length) return numTicks;
+    numTicks = num;
     return drawSlider;
   }
   
--- src/styles.js
+++ src/styles.js
@@ -2,24 +2,24 @@
 
 // styles
 export const roundEndsStyle = (selection) => selection.style('stroke-linecap', 'round');
-  
+
 export const trackStyle = (selection) => selection
-  .style('stroke', '#000')
-  .style('stroke-opacity', '0.3')
-  .style('strokeWidth', '10px');
+.style('stroke', '#000')
+.style('stroke-opacity', '0.3')
+.style('strokeWidth', '10px');
 
 export const trackInsetStyle = (selection) => selection
-  .style('stroke', '#ddd')
-  .style('stroke-width', 8);
+.style('stroke', '#ddd')
+.style('stroke-width', 8);
 
 export const trackOverlayStyle = (selection) => selection
-  .style('pointer-events', 'stroke')
-  .style('stroke-width', 50)
-  .style('stroke', 'transparent')
-  .style('cursor', 'crosshair');
-  
+.style('pointer-events', 'stroke')
+.style('stroke-width', 50)
+.style('stroke', 'transparent')
+.style('cursor', 'crosshair');
+
 export const handleStyle = (selection) => selection
-  .style('fill', '#fff')
-  .style('stroke', '#000')
-  .style('stroke-opacity', 0.5)
-  .style('strokeWidth', '1.25px');
+.style('fill', '#fff')
+.style('stroke', '#000')
+.style('stroke-opacity', 0.5)
+.style('strokeWidth', '1.25px');
